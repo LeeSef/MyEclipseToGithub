367b9f050b7797ca0f4069f4355c4b5d4c3694f3
LeeSef <liyuhui644504@qq.com>
2017-07-14 23:16:18 +0800

    First Edit Update Text

--- src/MyEclipseToGithub.java
+++ src/MyEclipseToGithub.java
@@ -4,7 +4,7 @@ public class MyEclipseToGithub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("这是一个测试程序");
+		System.out.println("这是一个测试程序");//?????第一次修改使用
 	}
 
 }
