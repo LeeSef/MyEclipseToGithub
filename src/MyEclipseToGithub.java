c2d362c9e97f3b2aef03055e67823ea216cba9dc
LeeSef <liyuhui644504@qq.com>
2017-07-18 12:40:54 +0800

    类的使用

--- src/MyEclipseToGithub.java
+++ src/MyEclipseToGithub.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 import Person.Person;
 
 public class MyEclipseToGithub {
+
+	private static Scanner scanner;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -8,12 +12,22 @@ public class MyEclipseToGithub {
 		System.out.println("这是一个测试程序");// 第一次修改使用
 
 		Person person = new Person();
-		person.eat();
+		
+		person.name = "小狗";
 		person.age = 33;
+		person.weight = 12.5f;
+		//吃饭
+		person.eat();
+		//打印年龄
 		int age = person.getAge();
-		System.out.print(age);
+		System.out.println(person.name+"的年龄是"+age);
+		//打印颜色
 		String color = person.getColor("Yellow");
-		System.out.println("\n" + color);
+		System.out.println("小狗的颜色是" + color);
+		scanner = new Scanner(System.in);
+		System.out.println("请输入小狗的颜色：");
+		color = scanner.next();
+		System.out.println("现在小狗的颜色是" + color);
 
 	}
 
